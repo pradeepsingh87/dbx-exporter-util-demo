d85015b8c23c6eb07d1078d130efccbf0525f439
pradeepsingh87 <pradeepsingh87>
2024-04-16 07:02:46 +0000

    Removed pandas install and restart python

--- notebooks/utils/excel_to_spark_df.py
+++ notebooks/utils/excel_to_spark_df.py
@@ -1,14 +1,4 @@
 # Databricks notebook source
-# Below function reads a excel file and returns a spark dataframe
-!pip install pandas
-
-
-# COMMAND ----------
-
-dbutils.library.restartPython()
-
-# COMMAND ----------
-
 
 def excel_to_spark_df(excel_file_path,rows_to_skip=0,sheet_name="Sheet1"):
     import pandas as pd
@@ -16,7 +6,3 @@ def excel_to_spark_df(excel_file_path,rows_to_skip=0,sheet_name="Sheet1"):
     spark_df = spark.createDataFrame(pandas_df)
     del pandas_df
     return spark_df
-
-# COMMAND ----------
-
-
